5e160703d4e6e5a4e051c9e02174b8a3ba33a203
ErJump <fedelijunior@gmail.com>
2022-12-20 10:47:24 +0100

    feat: :sparkles: Adds Category pojo

---
+++ src/main/java/org/generation/italy/demo/pojo/Category.java
@@ -0,0 +1,69 @@
+package org.generation.italy.demo.pojo;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotEmpty;
+
+@Entity
+@Table
+public class Category {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@NotEmpty(message = "il nome non può essere vuoto")
+	@Column(unique = true)
+	private String name;
+	
+	@ManyToMany(mappedBy = "categories")
+	@JsonIgnore
+	private List<Photo> photos;
+	
+	public Category() { }
+	public Category(String name) {
+		setName(name);
+	}
+	public Category(String name, List<Photo> photos) {
+		setName(name);
+		setPhotos(photos);
+	}
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public List<Photo> getPhotos() {
+		return photos;
+	}
+	public void setPhotos(List<Photo> photos) {
+		this.photos = photos;
+	}
+	
+	@Override
+	public String toString() {
+		return "(" + getId() + ") " + getName(); 
+	}
+	
+}
