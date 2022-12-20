23334515b9b191e960ef01d894ff926ca403b07d
ErJump <fedelijunior@gmail.com>
2022-12-20 16:07:33 +0100

    feat: :sparkles: Adds PhotoApiController

---
+++ src/main/java/org/generation/italy/demo/api/controller/PhotoApiController.java
@@ -0,0 +1,25 @@
+package org.generation.italy.demo.api.controller;
+
+import java.util.List;
+
+import org.generation.italy.demo.pojo.Photo;
+import org.generation.italy.demo.service.PhotoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/1/picture")
+@CrossOrigin("*")
+public class PhotoApiController {
+	
+	@Autowired
+	private PhotoService pS;
+	
+	@GetMapping("")
+	public List<Photo> getAll() {
+		return pS.findAll();
+	}
+}
