236130373532c82c0d4d0b5b81e8e58ea12eab45
ErJump <fedelijunior@gmail.com>
2022-12-20 14:04:25 +0100

    feat: :sparkles: Adds create and store methods

--- src/main/java/org/generation/italy/demo/controller/CategoryController.java
+++ src/main/java/org/generation/italy/demo/controller/CategoryController.java
@@ -7,8 +7,14 @@ import org.generation.italy.demo.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/category")
@@ -22,5 +28,24 @@ public class CategoryController {
 		List<Category> categories = cS.findAll();
 		model.addAttribute("categories", categories);
 		return "category-index";
+	}
+	
+	@GetMapping("/create")
+	public String create(Model model) {
+		Category c = new Category();
+		model.addAttribute("category", c);
+		return "category-create";
+	}
+	
+	@PostMapping("/create")
+	public String store(@Valid @ModelAttribute("category") Category category, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+		
+		if(bindingResult.hasErrors()) {
+			redirectAttributes.addFlashAttribute("errors", bindingResult.getAllErrors());
+			return "redirect:/category/create";
+		}
+		
+		cS.save(category);
+		return "redirect:/category";
 	}
 }
