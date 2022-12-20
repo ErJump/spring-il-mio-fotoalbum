d04271f3a78560d691bb72bf9ee853e6d7d63796
ErJump <fedelijunior@gmail.com>
2022-12-20 17:38:17 +0100

    feat: :sparkles: Adds CategoryApiController

---
+++ src/main/java/org/generation/italy/demo/api/controller/CategoryApiController.java
@@ -0,0 +1,26 @@
+package org.generation.italy.demo.api.controller;
+
+import java.util.List;
+
+import org.generation.italy.demo.pojo.Category;
+import org.generation.italy.demo.service.PhotoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/1/categorie")
+@CrossOrigin("*")
+public class CategoryApiController {
+	
+	@Autowired
+	private PhotoService pS;
+	
+	@GetMapping("/by/photo/{id}")
+	public List<Category> getCategoriesByPhotoId(@PathVariable("id") int id) {
+		return pS.findPhotoById(id).get().getCategories();
+	}
+}
