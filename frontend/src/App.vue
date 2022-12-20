dfe123264d57488fee3e1f904f00a1fa62af6baf
ErJump <fedelijunior@gmail.com>
2022-12-20 16:21:34 +0100

    feat: :sparkles: Adds vue frontend service + Init project

---
+++ frontend/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>Here we are BOYZ</h1>
+    <Main />
+  </div>
+</template>
+
+<script>
+import Main from './components/Main.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Main
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ frontend/src/components/Main.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Yo I'm Main</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Main',
+  
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
