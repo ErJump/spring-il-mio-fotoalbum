d8ac8f9320dd421cdedf6cdbbde998c4a353656d
ErJump <fedelijunior@gmail.com>
2022-12-20 17:04:34 +0100

    style: :lipstick: Adds header partial

---
+++ frontend/src/components/partials/Header.vue
@@ -0,0 +1,31 @@
+<template>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark px-3">
+            <a class="navbar-brand" href="#">Jump's Gallery</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                <a class="nav-item nav-link active" href="#">Home</a>
+                <a class="nav-item nav-link" href="http://localhost:8080/photo">Backoffice</a>
+                </div>
+            </div>
+        </nav>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'Header',
+    data() {
+        return {
+            
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/main/java/org/generation/italy/demo/api/controller/PhotoApiController.java
+++ src/main/java/org/generation/italy/demo/api/controller/PhotoApiController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/1/picture")
+@RequestMapping("/api/1/pictures")
 @CrossOrigin("*")
 public class PhotoApiController {
 	
