3155ea669e980cf0ac006730128a36284f18e2c7
ErJump <fedelijunior@gmail.com>
2022-12-20 10:48:09 +0100

    feat: :sparkles: Adds mtm relation between photo and category

--- src/main/java/org/generation/italy/demo/pojo/Photo.java
+++ src/main/java/org/generation/italy/demo/pojo/Photo.java
@@ -1,6 +1,10 @@
 package org.generation.italy.demo.pojo;
 
+import java.util.List;
+
 import org.hibernate.validator.constraints.URL;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -8,6 +12,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
@@ -41,13 +46,26 @@ public class Photo {
 	@NotNull(message = "la visibilità non può essere null")
 	private boolean visible;
 	
+	@ManyToMany
+	@JsonIgnore
+	private List<Category> categories;
+	
 	public Photo() { }
-	public Photo(String title, String description, String url, String tag, boolean visible) {
+	public Photo(String title, String description, String url, String tag, boolean visible, List<Category> categories) {
 		setTitle(title);
 		setDescription(description);
 		setUrl(url);
 		setTag(tag);
 		setVisible(visible);
+		setCategories(categories);
+	}
+	public Photo(String title, String description, String url, String tag, boolean visible, Category category) {
+		setTitle(title);
+		setDescription(description);
+		setUrl(url);
+		setTag(tag);
+		setVisible(visible);
+		addCategory(category);
 	}
 	
 	public int getId() {
@@ -98,10 +116,26 @@ public class Photo {
 		this.visible = visible;
 	}
 	
+	public void addCategory(Category category) {
+		boolean finded = false;
+		for (Category c : getCategories())
+			if (c.getId() == category.getId())
+				finded = true;
+		if(!finded)
+			getCategories().add(category);
+	}
+	
+	public List<Category> getCategories() {
+		return categories;
+	}
+	public void setCategories(List<Category> categories) {
+		this.categories = categories;
+	}
 	@Override
 	public String toString() {
-		return "(" + getId() + ") " + getTitle() + " - " 
-				+ getDescription() + " - " + getTag() + " - " 
-				+ isVisible() + " - " + getUrl();
+		return "(" + getId() + ") " + getTitle() + "\n" 
+				+ getDescription() + " - " + getTag() + "\n" 
+				+ isVisible() + " - " + getUrl() + "\n \t"
+				+ getCategories();
 	}
 }
