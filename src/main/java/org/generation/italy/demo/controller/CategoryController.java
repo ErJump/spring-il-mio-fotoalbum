97a363453a3709f3638559d57dae394e772d5921
ErJump <fedelijunior@gmail.com>
2022-12-20 14:20:23 +0100

    feat: :sparkles: Adds create and store methods in category controller

--- src/main/java/org/generation/italy/demo/controller/CategoryController.java
+++ src/main/java/org/generation/italy/demo/controller/CategoryController.java
@@ -3,6 +3,7 @@ package org.generation.italy.demo.controller;
 import java.util.List;
 
 import org.generation.italy.demo.pojo.Category;
+import org.generation.italy.demo.pojo.Photo;
 import org.generation.italy.demo.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -10,6 +11,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -48,4 +50,28 @@ public class CategoryController {
 		cS.save(category);
 		return "redirect:/category";
 	}
+	
+	@GetMapping("/edit/{id}")
+	public String edit(@PathVariable("id") int id, Model model) {
+		
+		Category c = cS.findCategoryById(id).get();
+		model.addAttribute("category", c);
+		return "category-edit";
+	}
+	
+	@PostMapping("/edit")
+	public String update(@Valid @ModelAttribute("category") Category category, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+		
+		if(bindingResult.hasErrors()) {
+			redirectAttributes.addFlashAttribute("errors", bindingResult.getAllErrors());
+			return "redirect:/category/edit/" + category.getId();
+		}
+		
+		List<Photo> oldCategoryPhotos = cS.findCategoryById(category.getId()).get().getPhotos();
+		category.setPhotos(oldCategoryPhotos);
+		cS.save(category);
+		
+		return "redirect:/category";
+	}
+	
 }
