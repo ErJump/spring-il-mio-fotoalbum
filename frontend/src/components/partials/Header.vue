e2be77eb0ac3d08531a3bda0a19de347c9cac584
ErJump <fedelijunior@gmail.com>
2022-12-21 12:03:01 +0100

    feat: :sparkles: Adds logo and icons in header

--- frontend/src/components/partials/Header.vue
+++ frontend/src/components/partials/Header.vue
@@ -1,10 +1,10 @@
 <template>
     <header>
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark px-3 d-flex flex-column align-items-start ms_bg_darker h-100 position-fixed">
-            <a class="navbar-brand text-primary my-4" href="#">Jump's Gallery</a>
+            <a class="navbar-brand text-primary my-4" href="#"><img class="ms_logo" src="../../assets/jg.png" alt="logo"></a>
             <div class="navbar-nav d-flex flex-column">
-                <a class="nav-item nav-link active" href="#">Home</a>
-                <a class="nav-item nav-link" href="http://localhost:8080/photo">Backoffice</a>
+                <a class="nav-item nav-link active" href="#"><i class="fa-solid fa-house display-inline-block me-2"></i> Home</a>
+                <a class="nav-item nav-link" href="http://localhost:8080/photo"><i class="fa-solid fa-toolbox display-inline-block me-2"></i> Backoffice</a>
             </div>
         </nav>
     </header>
@@ -24,5 +24,9 @@ export default {
 <style scoped>
     .ms_bg_darker{
         background-color: #1d1e1f !important;
+    }
+
+    .ms_logo{
+        height: 35px;
     }
 </style>
